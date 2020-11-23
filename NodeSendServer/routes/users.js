d8bb022cc3927d90389797e4567b6e32d9f15411
Esteban <estebanmejia277@gmail.com>
2020-11-23 14:57:14 -0500

    User validation

--- NodeSendServer/routes/users.js
+++ NodeSendServer/routes/users.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { check } = require('express-validator');
 
-router.post('/', 
+router.post('/',
+  [
+    check('name', 'Name required').not().isEmpty(),
+    check('email', 'Enter a valid email').isEmail(),
+    check('password', 'Password must be at least six characters long').isLength({ min: 6 }),
+
+
+  ],
   userController.newUser
 );
 
